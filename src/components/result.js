ee7dc01e34faff0758617e95fdccd52a8a2e63a4
Sajina <sajina@Sajinas-MacBook-Air.local>
2020-07-19 13:12:36 -0500

    initial

---
+++ src/components/result.js
@@ -0,0 +1,27 @@
+
+import React , {useState , useEffect} from 'react';
+import api from '../api/api';
+
+
+
+export default  () => {
+    const [results , setResults] = useState([]);
+    const [errorMessage , setErrorMessage] = useState('');
+    const getRepositories = async () => {
+        try{
+            console.log('get repositories called')
+            const repositories = await api.get();
+            console.log(repositories.data);
+            setResults(repositories);
+        }
+        catch(err){
+            setErrorMessage('Something went wrong');
+        }
+    }
+    
+    // useEffect(()=> {
+    //     performSearch('pasta');
+    // },[]);
+
+    return [getRepositories , results , errorMessage];
+}
